11a98c502875d0d6569cf384e04bd40f91ae9077
joethechickensource <wpcreswick@outlook.com>
2024-11-14 10:37:55 -0500

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,3 +1,4 @@
+/*
 function ajax(url,callback){
 	var ajaxRequest;
 	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
